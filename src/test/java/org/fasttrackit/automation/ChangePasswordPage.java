38999229866cac9ab4f11c4d44addc87dc716a4d
andreicoada <andreicoada1301@gmail.com>
2017-02-02 21:22:17 +0200

    Comis tot 26.01.2017

--- src/test/java/org/fasttrackit/automation/ChangePasswordPage.java
+++ src/test/java/org/fasttrackit/automation/ChangePasswordPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+
 public class ChangePasswordPage {
     @FindBy(name = "password")
     private  WebElement currentPasswordField;
@@ -15,12 +16,24 @@ public class ChangePasswordPage {
     private WebElement newPasswordField;
     @FindBy(name = "newPasswordRepeat")
     private WebElement repeatPasswordField;
-    //TODO save button
+    @FindBy(xpath = "//*[@id='preferences-win']//button[normalize-space(text())='Save']")
+    private WebElement saveBtn;
+    @FindBy(className = "status-msg")
+    private WebElement statusMsg;
 
-    public void changePassword(String currentPass,String newPass, String repeatNewPass) {
+    public  String getStatusMessage() {
+        String msg = statusMsg.getText();
+        System.out.println();
+        return msg;
+    }
+
+
+    public void update(String currentPass, String newPass, String repeatNewPass) {
         currentPasswordField.sendKeys(currentPass);
         newPasswordField.sendKeys(newPass);
         repeatPasswordField.sendKeys(repeatNewPass);
+        saveBtn.click();
     }
+
 
 }
--- src/test/java/org/fasttrackit/automation/ChangePasswordTest.java
+++ src/test/java/org/fasttrackit/automation/ChangePasswordTest.java
@@ -1,18 +1,25 @@
 package org.fasttrackit.automation;
 
 
+import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class ChangePasswordTest extends TestBase {
 
     private LoginPage loginPage;
+    private ChangePasswordPage changePasswordPage;
 
     public ChangePasswordTest (){
+
         loginPage = PageFactory.initElements(driver, LoginPage.class);
+        changePasswordPage = PageFactory.initElements(driver,ChangePasswordPage.class);
     }
 
 
@@ -25,13 +32,52 @@ public class ChangePasswordTest extends TestBase {
         WebElement preferencesBtn = driver.findElement(By.xpath("//button[@data-target='#preferences-win']"));
         preferencesBtn.click();
 
-        WebElement currentPasswordField = driver.findElement(By.name("password"));
-        WebElement newPasswordField = driver.findElement(By.name("newPassword"));
-        WebElement repeatPasswordField = driver.findElement(By.name("newPasswordRepeat"));
+        Utils.sleep(2000);
 
-        currentPasswordField.sendKeys("wrong.pass");
-        newPasswordField.sendKeys("new.pass");
-        repeatPasswordField.sendKeys("new.pass");
+        changePasswordPage.update("eu.pass", "acces", "acces");
+
+//        WebElement currentPasswordField = driver.findElement(By.name("password"));
+//        WebElement newPasswordField = driver.findElement(By.name("newPassword"));
+//        WebElement repeatPasswordField = driver.findElement(By.name("newPasswordRepeat"));
+//
+//        currentPasswordField.sendKeys("wrong.pass");
+//        newPasswordField.sendKeys("new.pass");
+//        repeatPasswordField.sendKeys("new.pass");
+        }
+
+
+    @Test
+    public void changePasswordWithValidPassword() {
+        openBrowser();
+
+        loginPage.login("eu@fast.com", "eu.pass");
+        WebElement preferencesBtn = driver.findElement(By.xpath("//button[@data-target='#preferences-win']"));
+        preferencesBtn.click();
+
+        Utils.sleep(2000);
+
+        changePasswordPage.update("eu.pass", "andrei", "andrei");
+        preferencesBtn.click();
+    }
+
+
+        public void changePasswordWithInvalidRepeatPassword() {
+            openBrowser();
+
+            loginPage.login("eu@fast.com", "eu.pass");
+            WebElement preferencesBtn = driver.findElement(By.xpath("//button[@data-target='#preferences-win']"));
+            preferencesBtn.click();
+            Utils.sleep(1000);
+
+            changePasswordPage.update("eu.pass", "new1.pass", "new2.pass");
+
+            assertThat(changePasswordPage.getStatusMessage(), is("Password does not match the confirm password"));
     }
 
 }
+
+
+
+
+
+
